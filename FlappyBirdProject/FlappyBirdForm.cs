3e3b712a88cc267f2f8b8989c668e6fbee3f3ba7
Soumya <satishsrinivasan0@gmail.com>
2021-02-13 10:47:33 +0000

    Added the flappy bird sprite.

--- FlappyBirdProject/FlappyBirdForm.cs
+++ FlappyBirdProject/FlappyBirdForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FlappyBirdProject
 {
-	public partial class Form1 : Form
+	public partial class FlappyBird : Form
 	{
-		public Form1()
+		public FlappyBird()
 		{
 			InitializeComponent();
 		}
